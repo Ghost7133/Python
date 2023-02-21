bd6277b96144928d17a5221168458b4b29063402
Ghost7133 <118304258+Ghost7133@users.noreply.github.com>
2023-02-21 12:59:40 +0300

    Delete дз-массивы directory

--- дз-массивы/1.js
+++
@@ -1,3 +0,0 @@
-numbers = [112312, 2123123345, 323445, Infinity, 6.2346789, 7, 8, 9, 0, 1]
-const maxvalue = Math.max(...numbers)
-console.log(maxvalue);
--- дз-массивы/2.js
+++
@@ -1,7 +0,0 @@
-for (let i = 0; i <= 15; i++) {
-if (i % 2) {
-    console.log(i, "число нечетное");
-} else {
-    console.log(i, "число четное");
-} 
-}
