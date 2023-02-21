0c68e2606bea9014505ef947bcf9b952a67b483d
Ghost7133 <118304258+Ghost7133@users.noreply.github.com>
2023-02-21 12:59:31 +0300

    Delete дз-булева directory

--- дз-булева/2.js
+++
@@ -1,3 +0,0 @@
-let c_year = 2024
-
-c_year % 4 ? console.log("год-не високосный") : console.log("год-високосный");
--- дз-булева/6.js
+++
@@ -1,5 +0,0 @@
-let myarray = [123213, 67, 123, 8234678]
-let a = myarray.sort(function(a,b){
-    return a - b
-})
-console.log(a);
