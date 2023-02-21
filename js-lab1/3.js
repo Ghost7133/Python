61e718fbc1ea572f5e38497c217958af22c4c2af
Ghost7133 <118304258+Ghost7133@users.noreply.github.com>
2023-02-21 12:59:00 +0300

    Delete js-lab1 directory

--- js-lab1/3.js
+++
@@ -1,9 +0,0 @@
-const firstnumber = 1;
-const secondnumber = 0;
-
-let max = Math.max(firstnumber, secondnumber);
-let min = Math.min(firstnumber, secondnumber);
-
-let random = Math.floor(Math.random() * Math.abs(max - min) + min);
-let finish = random + 1 - Math.abs(random % 2);
-console.log(finish);
