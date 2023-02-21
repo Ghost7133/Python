ddfbd672cbb3f0347b87e7a5a158df4607df80d3
Ghost7133 <118304258+Ghost7133@users.noreply.github.com>
2023-02-21 12:59:08 +0300

    Delete js-lab2 directory

--- js-lab2/1.js
+++
@@ -1,7 +0,0 @@
-const password = "sdhfjkhsd--hfjksh";
-
-password.lenght > 4
-  ? password.includes("_") || password.includes("-")
-    ? console.log("Пароль надежный")
-    : console.log("Пароль ненадежный")
-  : console.log("Пароль ненадежный");
