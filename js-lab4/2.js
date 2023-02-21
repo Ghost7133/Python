dc7f149d18d2f5901fcb8a05c40b855994965473
Ghost7133 <118304258+Ghost7133@users.noreply.github.com>
2023-02-21 12:59:23 +0300

    Delete js-lab4 directory

--- js-lab4/2.js
+++
@@ -1,9 +0,0 @@
-//С помощью цикла создать перевёрнутый вариант произвольной строки. Например, строка«Привет, мир!» должна превратиться в «!рим ,тевирП».
-const string = "Ghbdtn Vbh";
-let newstring = "";
-let i = 0;
-while (i < string.length) {
-  newstring += string[string.length - 1 - i];
-  i++;
-}
-console.log(newstring);
