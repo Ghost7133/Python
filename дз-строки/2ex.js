49d0d34b6e4f67f8fce45db0853502ac87e99906
Ghost7133 <118304258+Ghost7133@users.noreply.github.com>
2023-02-21 12:59:46 +0300

    Delete дз-строки directory

--- дз-строки/2ex.js
+++
@@ -1,9 +0,0 @@
-
-// Write a JavaScript function to convert a string into lower camel case
-// JavaScript Exercises -> javaScriptExercises
-const str = "JavaScript еxercises";
-
-console.log(
-  str.slice(0, str.indexOf(" ")) +
-    str.slice(str.indexOf(" ") + 1, str.indexOf(" ") + 2).toUpperCase() +
-    str.slice(str.indexOf(" ") + 2));
