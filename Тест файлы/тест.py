734b251736cfe91d7f3b19de5fb72c91b5fa017d
Ghost7133 <118304258+Ghost7133@users.noreply.github.com>
2023-03-01 11:09:13 +0300

    Add files via upload

---
+++ Тест файлы/тест.py
@@ -0,0 +1,18 @@
+def load_questions():
+    global questions
+    with open('input.txt', 'r') as f:
+        questions = eval(f.read())
+
+
+load_questions()
+ok = 0
+for q in questions.keys():
+    print(q)
+    print(*questions[q][0], sep='\n')
+    answ = input()
+    if answ in questions[q]:
+        print('Yes!')
+        ok += 1
+    else:
+        print('Wrong!')
+print('Правильных ответов: ', ok)
