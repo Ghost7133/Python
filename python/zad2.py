8a85beb79d9a0d8e33898bf09ea4d8e3ebd52934
Ghost7133 <118304258+Ghost7133@users.noreply.github.com>
2023-04-13 13:12:59 +0300

    Add files via upload

---
+++ python/zad2.py
@@ -0,0 +1,20 @@
+expression=input('введите выражение: ')
+for i in range(len(expression)):
+   if expression[i]=='+' or expression[i]=='-':
+       firstNumber=int(expression[:i])
+       expression=expression[i:]
+       break
+for i in range(1,len(expression)):
+   if expression[i]=='+' or expression[i]=='-':
+       secondNumber=int(expression[1:i])
+       thirdNumber=int(expression[i+1:])
+       break
+if expression[0]=='+':
+   answer=firstNumber+secondNumber
+else:
+   answer=firstNumber-secondNumber
+if expression[i]=='-':
+   answer=answer-thirdNumber
+else:
+   answer=answer+thirdNumber
+print('ответ:',answer)
